ced318df35caadce3aea88d9b6c76e9a5b7d4d7b
Troah Hewitt <t.hewitt2@saints.mhcc.edu>
2023-05-12 17:40:14 -0700

    mod 12 done

---
+++ module_12/sample_project/fitness-bootstrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Troah hewitt
+    File Name: script.js
+    Date: 05/12/23
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero'). height($(window).height());
+});
